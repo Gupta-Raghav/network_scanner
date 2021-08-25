f94855f066d2373f505434e6b2840c27f7583bba
Raghav <raghuweer23@gmail.com>
2021-08-25 14:15:29 +0100

    able to collect send and received packets without using arpping.

--- network_scanner.py
+++ network_scanner.py
@@ -4,6 +4,10 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_packet = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_packet_broadcast = broadcast/arp_packet
+    answered, unanswered = scapy.srp(arp_packet_broadcast, timeout=1)
+    print(answered.summary())
 
 scan("192.168.68.0/24")
